d2bbb11ce94a657e1f01e8653d52d1bf6db5c684
NabajitUtsab <noboutsab@gmail.com>
2026-05-18 17:28:31 +0600

    changed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://notes-management-system-1-jf7k.onrender.com";
 
 // ─────────────────────────────────────────────────────────────────────────────
 // HELPERS
